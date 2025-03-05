2bf9d99b1e396911dccc93d6559dd9d77728751d
zzo3ozz <wityoung.h@gmail.com>
2025-03-05 23:10:09 +0900

    init

---
+++ src/main/java/com/example/ikdaman/IkdamanApplication.java
@@ -0,0 +1,13 @@
+package com.example.ikdaman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkdamanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IkdamanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ikdaman/IkdamanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ikdaman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IkdamanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
