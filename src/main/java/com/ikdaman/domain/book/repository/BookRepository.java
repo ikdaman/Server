0e9647a2d443d362c4ba0e1994fa93c314f808fb
sonshn <sonshn@naver.com>
2025-05-04 11:19:09 +0900

    fix: Add MyBook API (중복 예외 처리 추가)

--- src/main/java/com/ikdaman/domain/book/repository/BookRepository.java
+++ src/main/java/com/ikdaman/domain/book/repository/BookRepository.java
@@ -4,7 +4,10 @@ import com.ikdaman.domain.book.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     Optional<Book> findByIsbn(String isbn);
+
+    // boolean existsMyBookByMemberIdAndBook(UUID memberId, Book Book);
 }
--- src/main/java/com/ikdaman/domain/mybook/service/MyBookServiceImpl.java
+++ src/main/java/com/ikdaman/domain/mybook/service/MyBookServiceImpl.java
@@ -50,6 +50,8 @@ public class MyBookServiceImpl implements MyBookService {
     @Override
     @Transactional
     public MyBookRes addMyBook(MyBookReq dto) {
+//        UUID memberId = UUID.fromString("d290f1ee-6c54-4b01-90e6-d701748f0851");
+
         Writer writer = writerRepository.findByWriterName(dto.getWriter())
                 .orElseGet(() -> writerRepository.save(
                         Writer.builder()
@@ -80,8 +82,13 @@ public class MyBookServiceImpl implements MyBookService {
 //        Member member = memberRepository.findByNickname(nickname)
 //                .orElseThrow(() -> new RuntimeException("회원이 존재하지 않습니다."));
 
+//        if(bookRepository.existsMyBookByMemberIdAndBook(memberId, book)) {
+//            throw new BaseException(MY_BOOK_ALREADY_EXISTS);
+//        }
+
         MyBook myBook = MyBook.builder()
 //                .memberId(member.getMemberId())
+//                .memberId(memberId)
                 .book(book)
                 .nowPage(0)
                 .isReading(true)
